3fc8cbed666ea3f6405fd9dc3c505c84cdcf1ed6
EarlyVacation <stuartamcleod@gmail.com>
2026-06-10 15:40:03 -0300

    Type fix

--- data/puzzles.ts
+++ data/puzzles.ts
@@ -84,4 +84,5 @@ export const PUZZLES: Puzzle[] = [
       '/puzzles/apple-welcome-home-2018/5.png',
     ],
     videoUrl: 'https://www.youtube.com/watch?v=Xwc22EK8qE0&list=RDXwc22EK8qE0&start_radio=1',
+   }
 ]
